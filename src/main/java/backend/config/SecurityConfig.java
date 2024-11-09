7ccaab121e39e93b665af07fe2247285d400f9d5
trivuniceva <trivunic99@gmail.com>
2024-11-09 23:04:37 +0100

    Create SecurityConfig.java

---
+++ src/main/java/backend/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource)) // Set CORS configuration
+                .csrf(csrf -> csrf.disable()) // Disable CSRF
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/**").permitAll() // Permit all requests to /api/**
+                        .anyRequest().authenticated()); // Any other request requires authentication
+        return http.build();
+    }
+}
