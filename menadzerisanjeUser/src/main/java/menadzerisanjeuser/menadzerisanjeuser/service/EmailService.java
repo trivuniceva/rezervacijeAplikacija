fc75b43125d28d42d5e9368e6838f3493d729d69
trivuniceva <trivunic99@gmail.com>
2024-11-11 10:21:52 +0100

    verification email basic

--- menadzerisanjeUser/src/main/java/menadzerisanjeuser/menadzerisanjeuser/service/EmailService.java
+++ menadzerisanjeUser/src/main/java/menadzerisanjeuser/menadzerisanjeuser/service/EmailService.java
@@ -1,41 +1,27 @@
 package menadzerisanjeuser.menadzerisanjeuser.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-//
-//    @Autowired
-//    private JavaMailSender mailSender;
-//
-//    public void sendActivationEmail(String email, String token) {
-//        System.out.println(token + "   " +  email);
-//        System.out.println("pocinje");
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setFrom("imenkoprezimic123@gmail.com");
-//        message.setTo(email);
-//        message.setSubject("Account Activation");
-//        message.setText("To activate your account, click the link below:\n\n" +
-//                "http://localhost:4200/activate-account?token=" + token);
-//        mailSender.send(message);
-//
-//        System.out.println("zavrsio");
-//    }
-//
-//    public void sendPaymentEmail(String email, String token) {
-//        System.out.println("pocinje slanje mejlaaaaaa");
-//
-////        paymentService.createPaymentToken(email, token);
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setFrom("imenkoprezimic123@gmail.com");
-//        message.setTo(email);
-//        message.setSubject("Payment Confirmation");
-//        message.setText("To confirm your payment, click the link below:\n\n" +
-//                "http://localhost:8080/api/pay/confirm?token=" + token);
-//        mailSender.send(message);
-//
-//        System.out.println("zavrsio mejlic bebeeee <3");
-//    }
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendActivationEmail(String email, String token) {
+        System.out.println(token + "   " +  email);
+        System.out.println("pocinje");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("imenkoprezimic123@gmail.com");
+        message.setTo(email);
+        message.setSubject("Account Activation");
+        message.setText("ћ ћаааао");
+        mailSender.send(message);
+
+        System.out.println("zavrsio");
+    }
 
 }
-
