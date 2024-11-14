d721910e2e062422c2fa526f82f9750a29959bad
trivuniceva <trivunic99@gmail.com>
2024-11-14 08:03:49 +0100

    view apartment details

---
+++ frontend/src/app/features/detailed-view/detailed-view.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-detailed-view',
+  standalone: true,
+  imports: [
+  ],
+  templateUrl: './detailed-view.component.html',
+  styleUrl: './detailed-view.component.css'
+})
+export class DetailedViewComponent {
+
+}
