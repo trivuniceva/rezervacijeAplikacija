38e7472f3824ed2286d34a7c4cdee8812e3de286
trivuniceva <trivunic99@gmail.com>
2024-11-14 01:16:42 +0100

    models for accommodation and reservation

---
+++ src/main/java/backend/model/Accommodation.java
@@ -0,0 +1,141 @@
+package backend.model;
+
+import jakarta.persistence.*;
+import menadzerisanjeuser.menadzerisanjeuser.model.User;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Entity
+@Table(name = "accommodations")
+public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    private String location;
+
+    @ElementCollection
+    private List<String> amenities;  // wifi, klima, itd.
+
+    @ElementCollection
+    private List<String> photos;
+
+    private int minGuests;
+    private int maxGuests;
+
+    @Enumerated(EnumType.STRING)
+    private AccommodationType type;
+
+    private boolean approved;
+
+    @OneToMany(mappedBy = "accommodation")
+    private List<Availability> availabilityList;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<String> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(List<String> amenities) {
+        this.amenities = amenities;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
+    }
+
+    public int getMinGuests() {
+        return minGuests;
+    }
+
+    public void setMinGuests(int minGuests) {
+        this.minGuests = minGuests;
+    }
+
+    public int getMaxGuests() {
+        return maxGuests;
+    }
+
+    public void setMaxGuests(int maxGuests) {
+        this.maxGuests = maxGuests;
+    }
+
+    public AccommodationType getType() {
+        return type;
+    }
+
+    public void setType(AccommodationType type) {
+        this.type = type;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public List<Availability> getAvailabilityList() {
+        return availabilityList;
+    }
+
+    public void setAvailabilityList(List<Availability> availabilityList) {
+        this.availabilityList = availabilityList;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
+
