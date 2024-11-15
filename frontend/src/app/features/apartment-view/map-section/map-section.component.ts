298dd9c465f0000fc5ec0d17dd65e7cac721af57
trivuniceva <trivunic99@gmail.com>
2024-11-15 12:30:17 +0100

    sredjen stil za bazu

--- frontend/src/app/features/apartment-view/map-section/map-section.component.ts
+++ frontend/src/app/features/apartment-view/map-section/map-section.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import * as L from 'leaflet';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 
 @Component({
   selector: 'app-map-section',
   standalone: true,
-  imports: [],
+  imports: [
+    LeafletModule
+  ],
   templateUrl: './map-section.component.html',
   styleUrl: './map-section.component.css'
 })
-export class MapSectionComponent {
+export class MapSectionComponent implements OnInit{
+  @Input() layers: L.Layer[] = [];
+  options: any;
+  map?: L.Map;
 
+  ngOnInit(): void {
+    this.options = this.getMapOptions();
+  }
+
+  private getMapOptions() {
+    return {
+      layers: [
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '© OpenStreetMap'
+        })
+      ],
+      zoom: 13,
+      center: L.latLng(45.2671, 19.8335)
+    };
+  }
 }
